ba8978969f11552b45a4a340fabdbbd7bc5beb31
Daria Jung <djj2115cu@gmail.com>
2014-03-12 15:59:45 -0400

    run with test cases

--- 6-037-proj-0.scm
+++ 6-037-proj-0.scm
@@ -4,6 +4,9 @@
   (+ (+ (expt x 4) (* 5 (expt x 2))) 4))
 
 
-(bitfunc 4)
-
+(bitfunc 0)  ;; Should be 4
+(bitfunc 1)  ;; Should be 10
+(bitfunc 2)  ;; Should also be 40
+(bitfunc -1) ;; Should be 10
+(bitfunc 10) ;; Should be pretty big, and positive, 10504
 
