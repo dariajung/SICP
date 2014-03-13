b2dc9e32716b89bd15e826d52ed8ce90174b6dad
Daria Jung <djj2115cu@gmail.com>
2014-03-12 21:51:12 -0400

    accidentally read equation as f(x) = x ^ 4 + 5 * x ^ 2 + 4 when it was f(x) = x ^ 4 - 5 * x ^ 2 + 4 changed from addition to subtraction

--- 6-037-proj-0.scm
+++ 6-037-proj-0.scm
@@ -1,14 +1,14 @@
-;; Implement f(x) = x ^ 4 + 5 * x ^ 2 + 4
+;; Implement f(x) = x ^ 4 - 5 * x ^ 2 + 4
 
 (define (bitfunc x)
-  (+ (+ (expt x 4) (* 5 (expt x 2))) 4))
+  (+ (- (expt x 4) (* 5 (expt x 2))) 4))
 
 
 (bitfunc 0)  ;; Should be 4
-(bitfunc 1)  ;; Should be 10
-(bitfunc 2)  ;; Should also be 40
-(bitfunc -1) ;; Should be 10
-(bitfunc 10) ;; Should be pretty big, and positive, 10504
+(bitfunc 1)  ;; Should be 0, or very close
+(bitfunc 2)  ;; Should also be very close to 0
+(bitfunc -1) ;; Should also also be very close to 0
+(bitfunc 10) ;; Should be pretty big, and positive
 
 (bitfunc 1.5)
 
